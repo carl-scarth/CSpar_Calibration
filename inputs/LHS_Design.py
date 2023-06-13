bbf4d187af4db29805babae03531f8e8852c815a
carl-scarth <carlscarth87@gmail.com>
2023-06-13 18:30:56 +0100

    Update to resolve conflicts in merge with master branch

--- inputs/LHS_Design.py
+++ inputs/LHS_Design.py
@@ -1,21 +1,22 @@
 import mogp_emulator as mogp
-from scipy.stats import norm, uniform
+from scipy.stats import norm, uniform, loguniform
 import numpy as np
 
 # Consider creating a folder called utils and importing the below classes separately as Jean does
 
 class Prior:
 # Define class of prior distribution objects containing type of distribution all important statistical attributes
-    def __init__(self,name, distribution, param_1, param_2):
+    def __init__(self,name, distribution, param_1, param_2, weight_LHS):
         # Initialise attributes of prior based upon the type of distribution
         self.name = name
         self.distribution = distribution
+        self.weight_LHS = weight_LHS
         if self.distribution == 'Gaussian':
             # If Gaussian, the prior is described by a mean and Coefficient of Variation
             self.mu = param_1
             self.COV = param_2
-        elif self.distribution == 'Uniform':
-            # If Uniform, the prior is described by a lower and upper bound
+        elif (self.distribution == 'Uniform') or (self.distribution == 'Loguniform'):
+            # If Uniform or loguniform, the prior is described by a lower and upper bound
             self.lb = param_1
             self.ub = param_2
 
@@ -23,19 +24,22 @@ class Prior:
     def sigma(self):
         if self.distribution == 'Gaussian':
             return self.mu*self.COV/100.0
+        else:
+            return None
+            # Shouldn't be used, but if necessary could actually specify this
         
     @property
     def min(self):
         if self.distribution == 'Gaussian':
             return self.mu - 3.0*self.sigma
-        elif self.distribution == 'Uniform':
+        elif (self.distribution == 'Uniform') or (self.distribution == 'Loguniform'):
             return self.lb
-    
+        
     @property
     def max(self):
         if self.distribution == 'Gaussian':
             return self.mu + 3.0*self.sigma
-        elif self.distribution == 'Uniform':
+        elif (self.distribution == 'Uniform') or (self.distribution == 'Loguniform'):
             return self.ub
 
 # Sample prior distributions using Latin Hypercube Sampling. Samples are weighted to encourage a space-filling design across intervals of equal probability
@@ -53,18 +57,16 @@ inputs = [
     ['t_ply','Gaussian', 0.196, 5.0]     # Mean taken from past data of material used in Bath, mean chosen from Engineering judgement of a conservative but reasonable spread in values
     ]
 
-# I considered doing this as a dictionary then changed my mind as the data wouldn't be ordered.
-# I've kept the code here in case I change it back again.  
-# inputs = {
-#    'E11'   : ['Gaussian', 115.6, 6.0],    # NIAR data, E11c, RTD 
-#    'E22'   : ['Gaussian', 9.24, 6.0],     # NIAR data, E22t, RTD
-#    'nu12'  : ['Uniform', 0.335, 12.123], # NIAR data, nu_12,c RTD. Not adjusting CoV as the observed value is quite high. Also consider using tensile measurement but adjusting up to 6% 
-#    'nu23'  : ['Gaussian', 0.487, 12.0],   # "Modelling and simulation methodology for unidirectional composite laminates in a Virtual Test Lab framework". Large CoV assumed as unspecified in the paper. Consider using smaller CoV
-#    'G12'   : ['Gaussian', 4.826, 6.0],    # NIAR data, G12,s RTD.
-#    't_ply' : ['Gaussian', 0.196, 5.0]     # Mean taken from past data of material used in Bath, mean chosen from Engineering judgement of a conservative but reasonable spread in values
-#    }
+# 3D Example
+inputs = [
+    ['E11', 'Gaussian', 115.6, 6.0],    # NIAR data, E11c, RTD 
+    ['t_ply', 'Gaussian', 0.196, 5.0],  # Mean taken from past data of material used in Bath, mean chosen from Engineering judgement of a conservative but reasonable spread in values
+    ['K', 'Loguniform', 100.0, 1.0e9]      # Bounds taken from rough paramteric study to check for switch from Simply-Supported to clamped behaviour
+    ]
 
-N = 60 # Number of samples to be generated
+# Also define a log-normal option as may be more appropriate for K given the sudden nature of the switch
+
+N = 30 # Number of samples to be generated
 d = len(inputs) # Number of inputs
 
 # Create a list of prior objects containing details of priors for each input
@@ -88,9 +90,23 @@ for i, item in enumerate(priors):
     elif item.distribution == 'Uniform':
         # Note, the scipy uniform distribution is bounded by [loc, loc + scale]
         xLHS[:,i] = [uniform.ppf(xij, loc = item.lb, scale = (item.ub - item.lb)) for xij in FLHS[:,i]]
+    elif item.distribution == 'Loguniform':
+        # In the loguniform distribution bounds a and b are specified as non-keyword arguments
+        xLHS[:,i] = [loguniform.ppf(xij, item.lb, item.ub) for xij in FLHS[:,i]]
 
 # Write Design of Experiments to a csv file
 head_string = ', '.join([item.name for item in priors])
-np.savetxt("LHSDesign" + str(N) + "x" + str(d) + ".csv", xLHS, delimiter=",", header = head_string, comments = "")
+
+# Check if output file exists to avoid over-writing
+filename = "LHSDesign" + str(N) + "x" + str(d) + ".csv"
+i = 1 # counter to be appended to filename if it already exists
+# Check if the filename is already in use, and if so append an integer to filename until a non-existing file is found
+while os.path.exists(filename):
+    filename = "LHSDesign" + str(N) + "x" + str(d) + "_" + str(i) + ".csv"
+    i = i + 1
+
+np.savetxt(filename, xLHS, delimiter=",", header = head_string, comments = "")
 # When using as in a top-level file it might be nice for each column to be attributed to a named variable. 
 # Don't bother for now as this will probably be easier when I know how to use Pandas
+# Consider scikit-opt for optimised Latin Hypercubes
+# Add a line to check I'm not over-writing existing data - I think I already have some somewhere
