317da6466acb08df7d30a3a3b146a67ebc5ca675
carl-scarth <carlscarth87@gmail.com>
2023-06-29 18:01:30 +0100

    added Force-Displacement plots

--- structure_outputs.py
+++ structure_outputs.py
@@ -1,15 +1,12 @@
 import numpy as np
 import pandas as pd
-
-
+import matplotlib.pyplot as plt
 
 file_str = "LHSDesign60x6"
-# file_str = "Problem_run"
 max_load = -250.0
 max_inc = 0.05
 displacements = np.loadtxt("inputs\\" + file_str + "_displacements_load=" + str(max_load) + "_max_inc=" + str(max_inc) +".csv",delimiter=",",skiprows=1)
-print(displacements.shape)
-sadsad
+
 #displacements = pd.read_csv("inputs\\LHSDesign60x6_displacements_load=-250.0_max_inc=0.1.csv",sep=",")
 RFs = np.loadtxt("inputs\\" + file_str + "_RFs_load=" + str(max_load) + "_max_inc=" + str(max_inc) + ".csv", delimiter=',', skiprows=1)
 with open ("inputs\\" + file_str + "_incs_load=" + str(max_load) + "_max_inc=" + str(max_inc) + ".txt",'r') as f:
@@ -23,63 +20,73 @@ frame_cnt = 0
 for i, sample in enumerate(increments):
     displacements_struct["Sample"].append({"Frame" : []})
     # displacements_df["Sample"].append(i) # Doesn't work. Can append a series though. Look at other ways of constructing pandas dataframes on the fly
-    for frame in sample:
+    for j, frame in enumerate(sample):
         displacements_struct["Sample"][i]["Frame"].append({"Increment" : frame,
                                                            "RFs" : RFs[3*frame_cnt:3*(frame_cnt+1)].tolist(),
                                                            "Displacements" : displacements[:,3*frame_cnt:3*(frame_cnt+1)]})
+        # Altermative format using Pandas dataframe
+        # Create numpy array using all of the data then create dataframe
+        uvw_df = pd.DataFrame(displacements[:,3*frame_cnt:3*(frame_cnt+1)], columns=["U","V","W"])
+        uvw_df["Sample"] = i
+        uvw_df["Frame"] = j
+        # etc. concatenate. Hopefully the headings will sort themselves out...
         frame_cnt += 1
-        
-    #print(sample)
 
-# displacements_struct["sample"][4]["frame"][-1]
+# Number of frames in each sample
+n_frames = [len(sample["Frame"]) for sample in displacements_struct["Sample"]]
+# Final increment for each sample (to check which runs didn't complete)
+end_incs = [sample["Frame"][-1]["Increment"] for sample in displacements_struct["Sample"]]
 
-displacements_struct["sample"][55] # This is the problem sample
+# Create a different structure with increments of 0.8 or lower
+# First create a copy of the dictionary to prevent modifying the original
+# This is slightly awkward as it's also necessary to change the entries in the dictionary
+displacements_struct_subset = displacements_struct.copy()
+displacements_struct_subset["Sample"] = displacements_struct_subset["Sample"].copy()
+displacements_struct_subset["Sample"] = [sample.copy() for sample in displacements_struct_subset["Sample"]]
+for sample in displacements_struct_subset["Sample"]:
+    # Pick 0.81 as the increment of interest is very slightly over 0.80
+    sample["Frame"] = [frame for frame in sample["Frame"] if frame["Increment"]<=0.81]
+    
+# Look at the number of frames and increment at the final frame to check that 
+# the following would be suitable for  an emulator trained using fixed increments
+n_frames_subset = [len(sample["Frame"]) for sample in displacements_struct_subset["Sample"]]
+end_incs_subset = [sample["Frame"][-1]["Increment"] for sample in displacements_struct_subset["Sample"]]
 
+# Plot force-displacement of reference point at the spar tip
+# Extract displacement (w) at reference point for all increments in each sample
+ref_w = [[-frame["Displacements"][1,2] for frame in sample["Frame"]]for sample in displacements_struct["Sample"]]
+RF_z = [[frame["RFs"][2] for frame in sample["Frame"]]for sample in displacements_struct["Sample"]]
 
-displacements_struct_all = displacements_struct
-disp_i = displacements_struct_all["sample"][56]["frame"][-1]#["Displacements"]
-disp_RP = []
-for sample in displacements_struct_all["sample"]:
-    disp_RP.append(sample["frame"][-1]["Displacements"][1,2])
+fig = plt.figure(figsize=(10,8))
+ax = fig.add_subplot(1, 1, 1)
+for i, sample in enumerate(ref_w):
+    ax.plot(sample, RF_z[i])
 
+label_font = {'family': 'serif', 'size': 16,}
+ax.set_ylabel("Force (kN)", fontdict = label_font)
+ax.set_xlabel("End axial displacement (mm)", fontdict = label_font)
 
-unique_disp_RP = set(disp_RP)
-len(unique_disp_RP)
-# An idea... Figure out exactly what to do with this later....
+max_u = [[frame["Displacements"][:,0].max() for frame in sample["Frame"]]for sample in displacements_struct["Sample"]]
+min_u = [[frame["Displacements"][:,0].min() for frame in sample["Frame"]]for sample in displacements_struct["Sample"]]
 
-# Provided that I have reasonable displacement data even for the cases in which
-# the analysis didn't complete, I can use this as emulator training data. Check
-# out force displacement curves to see if this is the case
-# Something funny happened with sample #56 (python index 55). Have a look at the
-# output I re=ran tp see what happened. IS it possible that python just re-loaded
-# saved outputs from previous sample. Check for this.
-# Plot load displacement curve at RP to get indication of the sort of behaviour
-# Also, research a bit more about dataframes
-# Fixed value 200kN, and nonlinear emulator codes can now be written around this.
-# For nonlinear treat load both as an input, but also do in the svd. See what
-# works better
+fig2 = plt.figure(figsize=(10,8))
+ax2 = fig2.add_subplot(1, 1, 1)
+for i, sample in enumerate(max_u):
+    ax2.plot(sample, RF_z[i])
 
-asdsdsad
+ax2.set_ylabel("Force (kN)", fontdict = label_font)
+ax2.set_xlabel("Maximum vertical displacement (mm)", fontdict = label_font)
 
-# Plot force-displacement of reference pointe eventually?
+fig3 = plt.figure(figsize=(10,8))
+ax3 = fig3.add_subplot(1, 1, 1)
+for i, sample in enumerate(min_u):
+    ax3.plot(sample, RF_z[i])
 
-infile = "inputs\\LHSDesign60x6"
-x_DoE = np.loadtxt(infile + ".csv", delimiter = ",", skiprows = 1, ndmin = 2)
-print(x_DoE[55,:])
-np.savetxt("Problem_run.csv", x_DoE[55,:].reshape((1,-1)), delimiter=",", header="Blank Line", )
+ax3.set_ylabel("Force (kN)", fontdict = label_font)
+ax3.set_xlabel("Minimum vertical displacement (mm)", fontdict = label_font)
 
-N, d = x_DoE.shape # Number of samples and number of inputs
-N_complete = 56
-x_DoE = x_DoE[N_complete:,:]
-iterable = enumerate(x_DoE, start=N_complete)
-for i, x_i in iterable:
-    print(i)
-    print(x_i)
+plt.show()
+asddsdsdsad
 
-with open ('RF_buffer.csv','r') as f:
-    RFs = [[float(RF) for RF in line.strip().split()] for line in f.readlines()]    
-
-
-n_incs = 0
-for run in increments:
-    n_incs += len(run)
+# Pandas
+# json?
