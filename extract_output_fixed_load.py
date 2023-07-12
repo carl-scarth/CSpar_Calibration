647a9f4544d90b388ff92d9d9107a91f2cfff6f3
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-07-12 16:49:01 +0100

    added file for extracting displacement at a fixed load from structured json output

---
+++ extract_output_fixed_load.py
@@ -0,0 +1,15 @@
+# Extracts output at a fixed load value from a json file
+import json
+import pandas as pd
+
+file_str = "LHSDesign40x4"
+with open("inputs\\" + file_str + "_output_struct.json",'r') as f:
+    output_dict = json.loads(f.readline())
+    
+load = 100 # Desired applied load Value
+out_frame = pd.DataFrame() # Create empty dataframe, the fill by looping over samples
+for i, sample in enumerate(output_dict["Sample"]):
+    disp_i = pd.DataFrame([frame for frame in sample["Frame"] if round(frame["RFs"][2]) == load][0]["Displacements"],columns=["u_"+str(i+1),"v_"+str(i+1),"w_"+str(i+1)])
+    out_frame = pd.concat((out_frame, disp_i), axis=1)
+
+out_frame.to_csv("inputs\\" + file_str +"_fixed_" + str(load) + "kN.csv", index=False)
