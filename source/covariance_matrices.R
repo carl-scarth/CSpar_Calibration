d7f7f8eeab5f3997c3766832a4db51ecbace695a
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-07-28 15:10:09 +0100

    added covariance for full-field emulator

--- source/covariance_matrices.R
+++ source/covariance_matrices.R
@@ -36,3 +36,56 @@ ARD_SE_cov_non_sym <- function(x1, x2, lambda, beta) {
   
   return(K)
 }
+
+full_field_cov = function(x, lambda, beta) {
+  # Auto covariance matrix for a full-field emulator composed of p basis 
+  # functions, for a set of N samples, resulting in a symmetric block diagonal 
+  # matrix with the ith block being a covariance matrix for the ith base.
+  # lambda is a vector of p emulator precisions, with ith entry for the ith base
+  # beta is a vector of p*q correlation lengths, where q is the dimension of x,
+  # with each q consecutive entries corresponding to a base
+  
+  print(lambda)
+  print(beta)
+  p = len(lambda) # Determine number of bases
+  print(p)
+  N = nrow(x) # Determine number of samples
+  d = ncol(x) # Determine number of dimensions of x
+  print(N)
+  print(q)
+  
+  K = matrix(0, N*p, N*p)
+  for (i in 1:p) {
+    # Calculate the covariance matrix for the ith base
+    K[((i-1)*N+1):(i*N), ((i-1)*N+1):(i*N)] = ARD_SE_cov(x, lambda[i], beta[((i-1)*d+1):(i*d)], 0)
+  }
+  
+  return(K)
+}
+
+full_field_cov_non_sym = function(x1, x2, lambda, beta) {
+  # Cross covariance function for a full-field emulator composed of p basis 
+  # functions, for two sets of sample inputs x1 and x2, resulting in a block 
+  # diagonal matrix with the ith diagonal being an N1 x N2 cross covariance
+  # matrix for the ith base.
+  
+  print(lambda)
+  print(beta)
+  print(x1)
+  print(x2)
+  p = len(lambda)
+  print(p)
+  N1 = nrow(x1)
+  N2 = nrow(x2)
+  d = ncol(x1)
+  print(N1)
+  print(N2)
+  print(d)
+  
+  K = matrix(0, N1*p, N2*p)
+  for (i in 1:p) {
+    K[((i-1)*N1+1):(i*N1),((i-1)*N2+1):(i*N2)] = ARD_SE_cov_non_sym(x1, x2, lambda[i], beta[((i-1)*d+1):(i*d)])
+  }
+  
+  return(K)
+}
