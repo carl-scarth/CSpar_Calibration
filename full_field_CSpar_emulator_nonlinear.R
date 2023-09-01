b7072f78962926802862e99f71519c3a68b426fa
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-09-01 14:50:48 +0100

    slight clarification of comments

--- full_field_CSpar_emulator_nonlinear.R
+++ full_field_CSpar_emulator_nonlinear.R
@@ -28,7 +28,7 @@ source("source/transform_input_output.R")
 
 # Set up parameters which govern the formulation
 
-p_eta = 11 # Number of basis functions to be retained for the emulator from SVD
+p_eta = 11 # Number of basis functions retained for the emulator from SVD
 exp_tol = 1e-6 # Tolerance variance fraction used to assess SVD convergence
 disp_str = "w" # String which identifies the displacement component of interest (u,v, or w)
 print_svd_output = TRUE # Print diagnostic output of svd to the terminal?
@@ -45,21 +45,19 @@ chains = 3 # Number of chains for simulation
 # Set up simulation data
 
 # Load in emulator training data input values from Design of Experiments. 
-in_file = "LHSDesign40x4" # File identifier string which is in both input and output files
+in_file = "LHSDesign40x4" # File identifier string for input and output files
 XT_sim = fread(paste("inputs/",in_file,".csv", sep = ""))
 
 # Determine useful quantities from model inputs and outputs. Variable names 
 # match the notation of Higdon et al. 2008
 q = ncol(XT_sim)          # number of calibration inputs
-tc = as.matrix(XT_sim)    # Convert training data input points to a matrix for passing to stan
+tc = as.matrix(XT_sim)    # Convert to a matrix for passing to stan
 m = nrow(XT_sim)          # sample size of computer simulation data
 
-# Load in  training data output displacement values from Abaqus. Here I've used
-# a similar naming convention to the inputs to automate changes. The file name 
-# can be changed manually if need be.
-# Here I've stored outputs structured in a json across samples and load increments
+# Load in training data output displacement values from Abaqus. I've used a
+# similar naming convention to the inputs to automate changes. 
+# Outputs are structured in a json across samples and load increments
 abaqus_displacements <- fromJSON(file = paste("inputs/",in_file,"_output_struct.json", sep=""))
-# DataFrame might be a more intuitive format for R...
 # Extract displacements from json, and store as matrix where each column is a 
 # training sample, and the rows are the concatenation of displacement output
 # across all output frames
