a022e1ea277ab37cf4e311648d2a267187cb5e47
carl-scarth <carlscarth87@gmail.com>
2023-09-05 14:01:32 +0100

    added normalisation

--- full_field_CSpar_calibration_nonlinear.R
+++ full_field_CSpar_calibration_nonlinear.R
@@ -19,6 +19,7 @@ setwd("C:/Users/cs2361/Documents/CSpar_Calibration/")
 
 # include functions which are called in this code
 source("source/abaqus_json.R")
+source("source/transform_input_output.R")
 # source("source/utils.R")
 # source("source/dimension_reduction.R")
 # source("source/prior_posterior_plots.R")
@@ -52,32 +53,37 @@ t_ply_mu = 0.196
 E11_cov = 6.0 
 t_ply_cov = 5.0
 # Bounds for log-uniform inputs
-K_lb = 100.0
-K_ub = 1.0e9
+# K_lb = 100.0
+# K_ub = 1.0e9
 
 # Define data_frame of prior parameters (this could be done via csv?)
-tf_param = data.frame(E11 = c("Gaussian",E11_mu, E11_mu*E11_cov/100),
-                      t_ply = c("Gaussian",t_ply_mu,t_ply_mu*t_ply_cov/100),
-                      K = c("Loguniform",log(K_lb),log(K_ub)))
+# tf_param <- data.frame(distribution = c("Gaussian","Gaussian","Loguniform"),
+#                       param_1 = c(E11_mu, t_ply_mu, log(K_lb)),
+#                       param_2 = c(E11_mu*E11_cov/100, t_ply_mu*t_ply_cov/100, log(K_ub)))
+# row.names(tf_param) <- c("E11","t_ply","log_K")
 
 # Additional pre-processing for flange-rotation example
 flange_theta_mu = 0.0
 flange_theta_sigma = 4.0
 # Define data_frame of prior parameters
-tf_param = data.frame(E11 = c("Gaussian",E11_mu, E11_mu*E11_cov/100),
-                      t_ply = c("Gaussian",t_ply_mu,t_ply_mu*t_ply_cov/100),
-                      LFlange_theta = c("Gaussian",flange_theta_mu,flange_theta_sigma),
-                      RFlange_theta = c("Gaussian",flange_theta_mu,flange_theta_sigma))
+tf_param <- data.frame(distribution = c("Gaussian","Gaussian","Gaussian","Gaussian"),
+                       param_1 = c(E11_mu, t_ply_mu, flange_theta_mu, flange_theta_mu ),
+                       param_2 = c(E11_mu*E11_cov/100, t_ply_mu*t_ply_cov/100, flange_theta_sigma, flange_theta_sigma))
+row.names(tf_param) <- c("E11","t_ply","LFlange_theta","RFlange_theta")
 
 #-------------------------------------------------------------------------------
-# Set up simulation data
+# Set up simulation data (need to retain this for normalisation of inputs)
 
-# Load in emulator training data (input values) from Design of Experiments. 
-# This input includes different values across uncontrolled calibration inputs.
-XT_sim = fread("inputs/LHSDesign30x3.csv")
-# take natural logarithm of spring stiffness
-XT_sim$K = log(XT_sim$K)
-colnames(XT_sim)[3] = "log_K"
+# Load in emulator training data input values from Design of Experiments. 
+# in_file = "LHSDesign50x3" # File identifier string for input and output csvs
+in_file = "LHSDesign40x4" # File identifier string for input and output csvs
+XT_sim = fread(paste("inputs/",in_file,".csv", sep = ""))
+
+# In this example I fit the emulator to the log of spring stiffness K, which is 
+# a more natural choice of values across which outputs are expected for
+# variations in this input
+# XT_sim$K = log(XT_sim$K)
+# colnames(XT_sim)[3] = "log_K"
 
 # Determine useful quantities from model inputs and outputs. Variable names 
 # match the notation of Higdon et al. 2008
@@ -97,5 +103,39 @@ dt_simulation = sorted_data[[1]]
 n_nodes = sorted_data[[2]] # Number of nodes
 n_frames = sorted_data[[3]] # Number of frames
 n_eta = nrow(dt_simulation) # total number of output points per simulation
+
+#------------------------------------------------------------------------------
+
+# All training data points are normalised onto the unit hypercube [0,1]^q before
+# being passed to stan. The same transformation is applied to the test points
+
+# Determine the maximum and minimum value of each input within the training data
+t_min = colMins(tc)
+t_max = colMaxs(tc)
+# Normalise the inputs using these maximum and minimum values
+tc = normalise_inputs(tc, t_min, t_max)
+
+# For all implemented distributions the transformation of the 1st parameter is the same
+tf_param$p1_trans = normalise_inputs(tf_param$param_1, t_min, t_max)
+# The transformation of the 2nd parameter is different if this is a standard deviation
+tf_param$p2_trans = NA
+for (i in 1:q){
+  # Second parameter of a Gaussian is a standard deviation
+  if (tf_param$distribution[i] == "Gaussian"){
+    tf_param$p2_trans[i] = normalise_inputs(tf_param$param_2[i], t_min[i], t_max[i], sd = TRUE)
+  } else if ((tf_param$distribution[i] == "Uniform") | (tf_param$distribution[i] == "Loguniform")){
+    tf_param$p2_trans[i] = normalise_inputs(tf_param$param_2[i], t_min[i], t_max[i])
+  } else {
+    stop("Error: Non-implemented distribution")
+  }
+}
+
+# plot Design of Experiments and test points
+pairs(tc,col="blue", pch=4, 
+      main = "Normalised Training Data Input values",
+      cex=1.5,
+      cex.labels = 1.75,
+      cex.axis=1.5,
+      cex.lab=1.5)
 
 #-------------------------------------------------------------------------------
