3920e410fa0ccd0bae8cd1d0fdfc4cf3b1099dee
carl-scarth <carlscarth87@gmail.com>
2023-09-05 12:35:52 +0100

    packaged input normalisation

--- source/transform_input_output.R
+++ source/transform_input_output.R
@@ -1,5 +1,22 @@
 # Code for scaling input and output samples before and after fitting the model
 
+# Function for normalising inputs onto the unit hypercube
+normalise_inputs <- function(x, x_min, x_max, sd = FALSE){
+  # Put maximum and minimum values into correct format
+  x_min = t(as.matrix(x_min))
+  x_max = t(as.matrix(x_max))
+  if (is.matrix(x)){
+    N = nrow(x)
+  } else {
+    N = 1
+  }
+  if (!sd){
+    x = x - x_min[rep(1,N),]
+  }
+  x = x/(x_max[rep(1,N),]-x_min[rep(1,N),])
+  return(x)
+}
+
 # Function for the inverse standardisation of vector output by the mean vector
 # mu_y, and scalar standard deviation sigma_y
 # sd indicates whether the y is a standard deviation, in which case it isn't
