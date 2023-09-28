18d2deadbb466a500c75a13e6fc56bd9f86f6df3
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-09-28 16:59:55 +0100

    fixed bug for interpolating matrices in nonlinear code

--- source/interpolate_data.R
+++ source/interpolate_data.R
@@ -73,11 +73,12 @@ intp_nodes_to_cloud_inc <- function(el_ind, gh, f_node, inc_ind, n_incs, connect
   # NOTE: It  isn't necessary that there are the same number of points in the 
   # cloud across all increments. It is assumed that there are the same number of
   # nodes across all increments.
-  f_cloud = c()
+  n_f = ncol(f_node)
+  f_cloud = matrix(0, nrow=0, ncol=n_f)
   for (i in 1:n_incs){
     print(i)
     f_i = as.matrix(f_node[((i-1)*n_nodes+1):(i*n_nodes),,drop=FALSE])
-    f_cloud = c(f_cloud, intp_nodes_to_cloud(el_ind[inc_ind == i],gh[inc_ind == i,], f_i, connectivity = connectivity, conn_file = conn_file, skip_nodes=skip_nodes))
+    f_cloud = rbind(f_cloud, intp_nodes_to_cloud(el_ind[inc_ind == i],gh[inc_ind == i,], f_i, connectivity = connectivity, conn_file = conn_file, skip_nodes=skip_nodes))
   }
 
   return(f_cloud)
