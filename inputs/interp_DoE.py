a9a97f2e273f65b922e9ce4c7a6d57951fc383fb
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2023-11-16 16:34:06 +0000

    created python interpolation code

--- inputs/interp_DoE.py
+++ inputs/interp_DoE.py
@@ -1,52 +1,16 @@
 # Interpolates model outputs for Design of Experiments to DIC coordinates
-from math import prod
-import numpy as np
+
 import pandas as pd
+import sys
+sys.path.append("..\\source")
 
-def intp_nodes_to_cloud(el_ind, gh, f_node, conn, GH = [], skip_nodes = 0):
-    # Interpolate nodal quantities f_node from nodes of a quad element mesh to 
-    # n_cloud point cloud coordinates. 
-    # el_ind = n_cloud vector of integer element indices, matching an element in
-    # the connectivity matrix
-    # gh = n_cloud x 2 matrix of element natural coordinates, with columns g and h
-    # f_node = n_nodes x n_f matrix of nodal values of n_f quantities of interest
-    # to be interpolated to point cloud coordinates
-    # conn = n_nodes x 4 matrix containing element connectivities
-    # GH = 2 x 4 matrix with natural coordinates of the 4 nodes connected to each
-    # element with rows g and h. If not specified default Abaqus values are used.
-    # skip_nodes = number of nodes to skip at the beginning of f_node This is 
-    # useful when the first set of points in the output are not referenced by the 
-    # connectivity file
-
-    if (skip_nodes > 0):
-        f_node = f_node[skip_nodes:,:]
-    
-    n_cloud = gh.shape[0] # Number of experimental data points
-    n_f = f_node.shape[1] # Number of output quantities to interpolate
-
-    # Define default Abaqus values for nodal natural coordinates, if not inputted
-    if len(GH) < 1:
-        #GH = np.array([[-1.0,1.0,1.0,-1.0],[-1.0,-1.0,1.0,1.0]])
-        GH = np.array([[-1.0,-1.0],[1.0,-1.0],[1.0,1.0],[-1.0,1.0]])
-    
-    # Interpolate using the functions for Abaqus isoparametric quad elements
-    f_cloud = np.empty((n_cloud,n_f))
-    for i, el in enumerate(el_ind):
-        # bases = 1.0 + gh[i,:]*GH.T
-        bases = 1.0 + gh[i,:]*GH
-        prod_bases = np.prod(bases, axis = 1, keepdims = True)*f_node[conn[el,:],:]
-        # bases = 1.0 + gh[i,:].T*GH # 1-dimensional basis functions
-        # prod_bases = np.prod(bases, axis=0)*f_node[conn[el,:],:]
-        # f_cloud[i,:] = np.sum(prod_bases, axis=1)/4.0
-        f_cloud[i,:] = np.sum(prod_bases, axis=0)/4.0
-
-    return(f_cloud)
+from interpolate_data import intp_nodes_to_cloud
 
 if __name__ == "__main__":
     # Load inputs from file
     conn_file = "nominal_shell_mesh_outer_surface_elements" # Connectivity
-    DoE_file = "LHSDesign40x4_fixed_200kN"                  # Model output
     exp_data_file = "Interpolated_DIC_200kN"                # Experimental data
+    DoE_file = "LHSDesign40x4_1_fixed_200kN"                  # Model output
     conn = pd.read_csv(conn_file+".csv").to_numpy(dtype=int)
     conn = conn - 1 # Convert to Python indexing from Abaqus
     model_out = pd.read_csv(DoE_file+".csv")
