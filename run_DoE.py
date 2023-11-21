6c2715558f700f0b0fc8c139bf1bf784b23bb919
carl-scarth <carlscarth87@gmail.com>
2023-11-21 17:11:25 +0000

    modified to accept log_K as an input

--- run_DoE.py
+++ run_DoE.py
@@ -30,8 +30,10 @@ def set_input(x_series, x_name, default_val):
 
 
 # infile = "inputs\\LHSDesign40x4_1"
+infile = "inputs\\LHSDesign100x9"
 # infile = "inputs\\LHSDesign75x7" # file in which DoE is stored
-infile = "inputs\\eccentricity_study"
+# infile = "inputs\\eccentricity_study_shell_E1T"
+# infile = "inputs\\eccentricities"
 
 # infile = "inputs\\LHSDesign100x6"
 # infile = "Problem_run"
@@ -42,37 +44,47 @@ infile = "inputs\\eccentricity_study"
 change_inc = True # Do I want to play with the increment size?
 write_buffer = False # Do I want to write a temporary file to store displacements as I go?
 restart = False # Am I restarting a previous analysis?
-shell_mesh = False # Is the mesh comprised of continuum shells?
-store_all_sam = True
-rotate_flanges = False
+shell_mesh = True # Is the mesh comprised of continuum shells?
+store_all_sam = False
+rotate_flanges = True
 
 max_inc = 0.05  # maximum increment
 init_inc = max_inc # initial increment. Set equal to maximum increment in the hope that this keeps the output regular
 min_inc = 1.0e-5 # minimum increment
-load = -350.0 # Applied load
+load = -250.0 # Applied load
 
-layup = [-45.0, 45.0, 90.0, 0.0, 0.0, 90.0, 45.0, -45.0]
-n_plies = len(layup) # Number of plies
+# sym = True # Representing only half of a symmetric layup
+sym = True
+# layup = [45.0, -45.0, 90.0, 0.0, 45.0, -45.0, 90.0, 0.0, 45.0, -45.0, 90.0, 0.0] # For shell mesh (doubling up on plies to reduce time as no failure in this model. Also there is symmetry)
+# layup = [-45.0, 45.0, 90.0, 0.0, 0.0, 90.0, 45.0, -45.0]
+# layup = [ -45.0, 45.0, 90.0, 0.0]
+# layup = [45.0, -45.0, -45.0, 45.0]
+layup = [45.0, -45.0, 45.0, -45.0, 45.0, -45.0, 0.0, 90.0, 0.0, 90.0, 0.0, 90.0] # Half layup for old CSpar
+if sym:
+    n_plies = 2*len(layup)
+else:
+    n_plies = len(layup) # Number of plies
 
-# Pandas version
+# Read in DoE
 x_DoE = pd.read_csv(infile + ".csv", sep = ",")
 print(x_DoE)
+
 N, d = x_DoE.shape # Number of samples and number of inputs
 
-# Update below with new numbers once I've seen mesh. Factor as multiple of ply numbers?
 if shell_mesh:
     n_Nodes = 9352
 else:
-    # n_Nodes = 116876 # Number of nodes per simulation - Old version before adding new reference nodes
     # n_Nodes = 116877 # Number of nodes per simulation (this increased with the additional BCs) 3D Mesh
     n_Nodes = (n_plies + 1)*4675 + 2 # Assumes mesh density is fixed
 
 # Define inputs which are held constant
 model_name = 'CSpar_sam' # Name of the Abaqus model
 Zlength = 420.0 # effective length of the spar (between end blocks).
-# Update when ply thickness known
-# height = 55.0 # distance from tip of the flanges to the outer surface of the web
-height = 61.0 # distance from tip of the flanges to the outer surface of the web
+# Check if this is the same for a shell mesh. Probably is based on below
+height = 55.0 # distance from tip of the flanges to the outer surface of the web
+# height = 61.0 # distance from tip of the flanges to the outer surface of the web
+# rotation_offset = 100.0
+rotation_offset = 0.0
 
 # If we are restarting an aborted set of runs, then reload existing data, otherwise initialise entities used to store data
 if restart:
@@ -135,11 +147,11 @@ for i, x_i in iterable:
     # Pick different input file generating script depending on if the mesh is comprised of shells or not
     # ideally I'd just use the same file taking a Boolean input, but keep for now in case of changes
     if shell_mesh:
-        write_shell_parameters(t_ply=t_ply, Zlength=Zlength, height=height, LFlange_theta=LFlange_theta, RFlange_theta=RFlange_theta,  model_name=file_str)
+        write_shell_parameters(t_ply=t_ply, Zlength=Zlength, height=height, LFlange_theta=LFlange_theta, RFlange_theta=RFlange_theta, n_plies=n_plies, rotate_flanges = rotate_flanges, model_name=file_str)
     else:
         # Write parameters to a text file which will be used to generate mesh
         # write_parameters(t_ply = t_ply, Zlength = Zlength, height = height, model_name = model_name)
-        write_parameters(t_ply = t_ply, Zlength = Zlength, height = height, StackSeq = layup, nb_plies = n_plies, rotate_flanges = rotate_flanges, model_name = file_str)
+        write_parameters(t_ply = t_ply, Zlength = Zlength, height = height, StackSeq = layup, rotate_flanges = rotate_flanges, model_name = file_str)
 
     # Generate the mesh using Gmsh
     write_mesh()
@@ -153,44 +165,55 @@ for i, x_i in iterable:
     nu12 = set_input(x_i, "nu12", 0.335)
     nu23 = set_input(x_i, "nu23", 0.487)
     G12 = set_input(x_i, "G12", 4.826)
-    K = set_input(x_i, "K", 1.0e10)
     x_spring = set_input(x_i, "x_spring", 27.5)
+    if "K" in x_i:
+        K = x_i["K"]
+    elif "log_K" in x_i:
+        K = np.exp(x_i["log_K"])
+    else:
+        K = 1.0e10
     
     # write Abaqus input file using the appropriate function, depending on whether or not the mesh is 
     # comprised of shells
     if shell_mesh:
-        write_shell_inp(E11=E11, E22=E22, nu12=nu12, nu23=nu23, G12=G12, t_ply=t_ply, K=K, x_spring=x_spring, load=load, init_inc=init_inc, min_inc=min_inc, max_inc=max_inc)
+        write_shell_inp(E11=E11, E22=E22, nu12=nu12, nu23=nu23, G12=G12, t_ply=t_ply, K=K, x_spring=x_spring, load=load, StackSeq = layup, init_inc=init_inc, min_inc=min_inc, max_inc=max_inc)
     else:
-        write_inp(E11=E11, E22 = E22, nu12 = nu12, nu23 = nu23, G12 = G12, K=K, x_spring=x_spring, load=load, init_inc=init_inc, min_inc=min_inc, max_inc=max_inc)
-    
+        write_inp(E11=E11, E22 = E22, nu12 = nu12, nu23 = nu23, G12 = G12, K=K, x_spring=x_spring, load=load, rotation_offset = rotation_offset, init_inc=init_inc, min_inc=min_inc, max_inc=max_inc)
+
     # Run Abaqus from the command line
-    command = "Abaqus Job=" + model_name + " input=\"Abaqus\\" + model_name + ".inp\" interactive ask_delete=OFF cpus=2"
+    command = "Abaqus Job=" + file_str + " input=\"Abaqus\\" + file_str + ".inp\" interactive ask_delete=OFF cpus=2"
+    print(command)
     os.system(command)
+    
 
     # Process the abaqus output to extract displacements and nodal coordinates
     if change_inc:
         # If multiple frames are requested, run the code which processes all frames
-        command = "abaqus viewer noGUI=process_outputs_all_frames.py -- " + model_name
+        command = "abaqus viewer noGUI=process_outputs_all_frames.py -- " + file_str
     else:
         # If only one increment is required just run the script which processes the final frame
-        command = "abaqus viewer noGUI=process_outputs.py -- " + model_name
+        command = "abaqus viewer noGUI=process_outputs.py -- " + file_str
     
     os.system(command)
 
     # The increment index outputted by the postprocessing file should help keep track of things.
     # At some point consider looking at numpy options for 3d arrays, and json files. This is going to be an 
     # issue for the experimental data as well so worth getting a decent structure set downl
-    disp_i = np.loadtxt(model_name + "_displacement.csv", delimiter = ",", skiprows = 0)
+    # disp_i = np.loadtxt(model_name + "_displacement.csv", delimiter = ",", skiprows = 0)
+    disp_i = np.loadtxt(file_str + "_displacement.csv", delimiter = ",", skiprows = 0)
     # nodes_i = np.loadtxt(model_name + "_nodes.csv", delimiter = ",", skiprows = 0)
     if change_inc:
         displacements = np.concatenate((displacements,disp_i), axis=1)
         # Also load in increment indices and reaction forces
-        incs_i = np.loadtxt(model_name + "_increments.csv", delimiter = ",", skiprows = 0, ndmin = 1)
+        #incs_i = np.loadtxt(model_name + "_increments.csv", delimiter = ",", skiprows = 0, ndmin = 1)
+        incs_i = np.loadtxt(file_str + "_increments.csv", delimiter = ",", skiprows = 0, ndmin = 1)
         incs.append(incs_i.tolist())
-        RFs_i = np.loadtxt(model_name + "_RFs.csv", delimiter = ",", skiprows = 0)
+        # RFs_i = np.loadtxt(model_name + "_RFs.csv", delimiter = ",", skiprows = 0)
+        RFs_i = np.loadtxt(file_str + "_RFs.csv", delimiter = ",", skiprows = 0)
         RFs = np.concatenate((RFs, RFs_i.reshape((1, -1))), axis=1)
         # Load in json file if using
-        with open(model_name + "_output.json", "r") as f:
+        # with open(model_name + "_output.json", "r") as f:
+        with open(file_str + "_output.json", "r") as f:
             # Load in string from file
             sample_dict = json.loads(f.readline())
         
@@ -213,7 +236,7 @@ for i, x_i in iterable:
             np.savetxt(f, incs_i.reshape((1,-1)))
         with open('RF_buffer.csv', 'a') as f:
             np.savetxt(f, RFs_i.reshape(1,-1))
-    asdsadsd    
+    
 # Consider using json format, stick with csv for now. This should be relatively easy if working with python dictionaries
 
 # Write model outputs from all simulations to csv files   
