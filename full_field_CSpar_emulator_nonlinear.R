1c1571ffbf9f912029eea6de0c279db4c3e30a24
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2024-01-29 10:16:29 +0000

    modified to allow standardisation via displacement component specific standard deviation

--- full_field_CSpar_emulator_nonlinear.R
+++ full_field_CSpar_emulator_nonlinear.R
@@ -31,6 +31,9 @@ source("source/transform_input_output.R")
 p_eta = 20 # Number of basis functions retained for the emulator from SVD
 exp_tol = 1e-6 # Tolerance variance fraction used to assess SVD convergence
 disp_str = c("u","v","w") # String which identifies the displacement component of interest (u,v, or w) or list of multiple
+# disp_str = c("u","w")
+# disp_str = "u"
+q_y = length(disp_str)
 print_svd_output = TRUE # Print diagnostic output of svd to the terminal?
 export_modes = TRUE # Calculate modes of emulator hyperparameters and write to file?
 # Define parameters of the gamma prior on the error associated with truncating
@@ -45,7 +48,7 @@ chains = 3 # Number of chains for simulation
 # Set up simulation data
 
 # Load in emulator training data input values from Design of Experiments. 
-in_file = "LHSDesign40x4" # File identifier string for input and output files
+in_file = "LHSDesign70x7" # File identifier string for input and output files
 XT_sim = fread(paste("inputs/",in_file,".csv", sep = ""))
 
 # Determine useful quantities from model inputs and outputs. Variable names 
@@ -57,8 +60,8 @@ m = nrow(XT_sim)          # sample size of computer simulation data
 # Load in training data output displacement values from Abaqus. I've used a
 # similar naming convention to the inputs to automate changes. 
 # Outputs are structured in a json across samples and load increments
-abaqus_displacements <- fromJSON(file = paste("inputs/",in_file,"_output_struct.json", sep=""))
-# abaqus_displacements <- fromJSON(file = paste("inputs/",in_file,"_downsam_2.json", sep=""))
+# abaqus_displacements <- fromJSON(file = paste("inputs/",in_file,"_output_struct.json", sep=""))
+abaqus_displacements <- fromJSON(file = paste("inputs/",in_file,"_downsam_2.json", sep=""))
 # Extract displacements from json, and store as matrix where each column is a 
 # training sample, and the rows are the concatenation of displacement output
 # across all output frames
@@ -71,7 +74,7 @@ n_eta = nrow(dt_simulation) # total number of output points per simulation
 #-------------------------------------------------------------------------------
 
 # Load in test points at which predictions are required
-XT_pred = fread("inputs/LHSDesign40x4_1.csv")
+XT_pred = fread("inputs/LHSDesign70x7_1.csv")
 t_pred = as.matrix(XT_pred)
 n_pred = nrow(t_pred) # number of predictions
 
@@ -107,7 +110,7 @@ pairs(t_pred,col = "blue", pch=4,
 # (i.e. zero mean at each node), and overall standard deviation of 1, then 
 # decomposed according to basis vectors K_eta
 
-outlist = standardise_vector_output(dt_simulation)
+outlist = standardise_vector_output(dt_simulation, q_y = q_y)
 eta = as.matrix(outlist[[1]]) # Convert to matrix for stan
 mu_dt = outlist[[2]]
 sd_dt = outlist[[3]]
@@ -176,8 +179,8 @@ labels = colnames(XT_sim) # don't think I need this but keeping just in case
 # If required, estimate the modes of emulator hyperparameters and write to a csv
 if (export_modes){
   modes = full_field_emulator_modes(rho_w, lambda_w, lambda_eta)
-  # write.csv(modes, paste("outputs/nonlinear_emulator_modes_",in_file,".csv", sep=""), row.names = FALSE)
-  write.csv(modes, paste("outputs/nonlinear_emulator_modes_",in_file,"_uvw.csv", sep=""), row.names = FALSE)
+  #write.csv(modes, paste("outputs/nonlinear_emulator_modes_",in_file,".csv", sep=""), row.names = FALSE)
+  write.csv(modes, paste("outputs/nonlinear_emulator_modes_",in_file,"_w.csv", sep=""), row.names = FALSE)
 }
 
 # Plot correlation parameter histograms
@@ -191,8 +194,9 @@ lambda_hist(lambda_eta, prior_shape = a_eta, prior_rate = b_eta, label = "lambda
 
 # Make predictions from fitted Gaussian process emulator, and write these
 # predictions to a .json file
+# MODIFY PREDICTION CODE TO PROPERLY DO INVERSE TRANSFORMATION WITH UPDATED CODE
 
-N_sam_pred = 200 # Required number of prediction samples
+N_sam_pred = 100 # Required number of prediction samples
 # Make predictions. Request only averages of the full-field across the posterior
 # uncertainty
 out_list = full_field_gp_pred(N_sam_pred, tc, z_hat, t_pred, beta_w, lambda_w, lambda_eta, K_eta, KTK_inv, sam_gp = FALSE, output_coeff_sam = FALSE, output_ff_sam = FALSE, output_coeff_mean = FALSE, output_ff_mean = TRUE)
--- source/transform_input_output.R
+++ source/transform_input_output.R
@@ -46,7 +46,7 @@ rescale_inputs <- function(x_norm, x_min, x_max, std = FALSE){
 # necessary to centre before scaling
 # There is an option to provide the mean vector and standard deviation, 
 # otherwise these are calculated internally
-standardise_vector_output <- function(y, mu_y = NULL, sigma_y = NULL, std = FALSE){
+standardise_vector_output <- function(y, mu_y = NULL, sigma_y = NULL, std = FALSE, q_y = 1){
   if (!std) {
     if (is.null(mu_y)){
       mu_y = rowMeans(y)
@@ -57,7 +57,16 @@ standardise_vector_output <- function(y, mu_y = NULL, sigma_y = NULL, std = FALS
     # Note, it's better to calculate this after centring the data, as this 
     # affects the standard deviation (given the mean vector is used, rather than
     # the overall mean)
-    sigma_y = sd(y)
+    if (q_y > 1) {
+      simga_y = c()
+      n_y = nrow(y)/q_y
+      for (i in 1:q_y) {
+        y_i = y[((i-1)*n_y+1):(i*n_y),]
+        sigma_y = c(sigma_y, rep(sd(y_i),n_y))
+      }
+    } else {
+      sigma_y = sd(y)
+    }
   }
   if (length(sigma_y) == 1) {
     y_scale = y/sigma_y
@@ -66,6 +75,7 @@ standardise_vector_output <- function(y, mu_y = NULL, sigma_y = NULL, std = FALS
   }
   return(list(y_scale, mu_y, sigma_y))
 }
+
 
 # Function for the inverse standardisation of vector output by the mean vector
 # mu_y, and scalar standard deviation sigma_y
