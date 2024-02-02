26f8e80f86f52b2e291144b0c097519e8c54e58a
carl-scarth <carlscarth87@gmail.com>
2024-02-02 17:21:16 +0000

    added some examples for new spars

--- inputs/generate_DoE.py
+++ inputs/generate_DoE.py
@@ -92,8 +92,31 @@ inputs = [
     ['K', 'Lognormal', 16.0, 1.0, True],   # Bounds taken from rough paramteric study to check for switch from Simply-Supported to clamped behaviour
     ]
 
-N = 100 # Number of samples to be generated
-xLHS = transformed_LHS(inputs, N, sampler_package="scikit-optimize", sampler_kwargs={"lhs_type":"classic","criterion":"maximin", "iterations":10000})
+# Example for new spar (IM7 properties)
+# Simple example assuming no misalignment in x direction
+inputs = [
+    ['E11', 'Gaussian', 140.9, 6.0, True], # NIAR, RTD
+    #['G12', 'Gaussian', 4.69, 6.0, True], # NIAR, RTD
+    ['t_ply', 'Gaussian', 0.125, 4.0, True], #  Meng Yi's Refs
+    ['K', 'Loguniform', 10.0, 25000.0, True], # Initial test case - switch probably to log-gamma once I've had time to think about it
+    #['x_spring_error', 'Uniform', -1.0, 1.0, True]  # Halfway between two settings
+    #['pivot_offset_error', 'Uniform', -5.0, 5.0, True] # Rig is precision machined, pick this value to incorporate uncertainty in trimmed spar length, and amount embedded in end caps. Basically this changes the effective length
+]
+
+inputs = [
+    ['E11', 'Gaussian', 140.9, 6.0, True], # NIAR, RTD
+    ['G12', 'Gaussian', 4.69, 6.0, True], # NIAR, RTD
+    ['t_ply', 'Gaussian', 0.125, 4.0, True], #  Meng Yi's Refs
+    ['K', 'Loggamma', 1.0, 0.3, True], # Initial test case - switch probably to log-gamma once I've had time to think about it
+    ['x_spring_error', 'Uniform', -1.0, 1.0, True],  # Halfway between two settings
+    ['pivot_offset_error', 'Uniform', -5.0, 5.0, True] # Rig is precision machined, pick this value to incorporate uncertainty in trimmed spar length, and amount embedded in end caps. Basically this changes the effective length
+]
+# Write code for outputting prior info
+# The above looks at getting eccentricty wrong uniformly. Also look at doing a misalignment if necessary. Also consider parametrising, e.g. offset misaligment + slope*l/2 or somethign
+# Look for an option with asymmetric x... not sure about y?
+
+N = 5000 # Number of samples to be generated
+xLHS = transformed_LHS(inputs, N)#, sampler_package="scikit-optimize", sampler_kwargs={"lhs_type":"classic","criterion":"maximin", "iterations":10000})
 d = len(inputs) # Number of inputs
 
 # If a log distribution, output the natural log as this is a more natural scale for the Gaussian process
