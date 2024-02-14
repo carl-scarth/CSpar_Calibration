c9de037cdd12b436018bf6ff1c16fe4d6ae03a41
carl-scarth <carlscarth87@gmail.com>
2024-02-14 12:12:40 +0000

    including displacement control eg

--- inputs/plot_force_displacement.py
+++ inputs/plot_force_displacement.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 infile = "nominal_inputs_new_spar"
-output_file = infile + "_output_struct.json"
+output_file = infile + "_output_struct.json" 
+output_file_2 = infile + "_output_struct_disp.json"
 RP_node = 1 # node index for reference point where load is applied
 labels = ["NIAR database properties", "Meng Yi Properties"]
 
@@ -11,12 +12,19 @@ labels = ["NIAR database properties", "Meng Yi Properties"]
 with open(output_file, "r") as f:
     # Load in string from file
     in_dict = json.loads(f.readline())  
+with open(output_file_2, "r") as f:
+    # Load in string from file
+    in_dict_2 = json.loads(f.readline())  
 
 RFs, w_RP = ([], [])
 for sample in in_dict["Sample"]:
     RFs.append([frame["RFs"][2] for frame in sample["Frame"]])
     w_RP.append([-frame["Displacements"][RP_node][2] for frame in sample["Frame"]])
 
+RFs2, w_RP2 = ([], [])
+for sample in in_dict_2["Sample"]:
+    RFs2.append([frame["RFs"][2] for frame in sample["Frame"]])
+    w_RP2.append([-frame["Displacements"][RP_node][2] for frame in sample["Frame"]])
 # print(w_RP)
 
 # Load in Meng Yi Force displacement from the fileshare
@@ -29,6 +37,10 @@ fig, ax = plt.subplots(figsize=(10,4))
 max_load = 300.0
 for force, disp, label in zip(RFs, w_RP, labels):
     ax.plot(disp, force, linewidth=2.0, label=label)
+
+for force, disp, label in zip(RFs2, w_RP2, labels):
+    ax.plot(disp, force, linewidth=2.0, label=label+"_disp_control")
+    
 ax.plot(mengyi_fd[:,0], mengyi_fd[:,1], linewidth=2.0, label = "MengYi Model")
 ax.plot(exp_fd[:,0], -exp_fd[:,1], "bx", label = "Experiment") 
 
