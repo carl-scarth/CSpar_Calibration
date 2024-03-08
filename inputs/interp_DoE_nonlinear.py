c5c4306b90ef647f1ba349aeee9193a5d3d7b980
Carl Scarth <91951049+carl-scarth@users.noreply.github.com>
2024-03-08 16:31:50 +0000

    updated for outputting displacement at end of new spars

--- inputs/interp_DoE_nonlinear.py
+++ inputs/interp_DoE_nonlinear.py
@@ -10,9 +10,10 @@ from interpolate_data import intp_nodes_to_cloud
 
 if __name__ == "__main__":
     # Load inputs from file
-    in_file = "LHSDesign60x6_2"
+    in_file = "LHSDesign50x5_1"
     conn_file = "nominal_shell_mesh_outer_surface_elements" # Connectivity
-    exp_data_file = "Selected_Points_CS02P"                 # Experimental data
+    # exp_data_file = "Selected_Points_CS02P"                 # Experimental data
+    exp_data_file = "mid_point"                 # Experimental data
     DoE_file = in_file + "_output_struct_disp.json"         # Model output
     conn = pd.read_csv(conn_file+".csv").to_numpy(dtype=int)
     conn = conn - 1 # Convert to Python indexing from Abaqus
@@ -35,12 +36,14 @@ if __name__ == "__main__":
         if output_mean:
             newcol = np.empty([len(sample["Frame"]),1])
             force = np.empty([len(sample["Frame"]),1])
+            crosshead = np.empty([len(sample["Frame"]),1])
         for j,frame in enumerate(sample["Frame"]):
             interp_outputs_i = intp_nodes_to_cloud(el_ind, gh, np.array(frame["Displacements"]), conn, skip_nodes = 2)
             interp_outputs_i = pd.DataFrame(interp_outputs_i, columns = ["u_interp", "v_interp", "w_interp"])
             if output_mean:
                 newcol[j,0] = interp_outputs_i["w_interp"].mean()
                 force[j,0] = frame["RFs"][2]
+                crosshead[j,0] = frame["Displacements"][1][2]
             else:
                 interp_outputs_i = pd.concat([exp_data[['index','x_proj','y_proj','z_proj','x_rot','y_rot','z_rot']],interp_outputs_i],axis=1)
                 interp_outputs_i["Sample"] = i
@@ -49,8 +52,6 @@ if __name__ == "__main__":
                 interp_outputs = pd.concat([interp_outputs, interp_outputs_i],axis=0,ignore_index=True)
 
         if output_mean:
-            print(pd.DataFrame(newcol, columns = ["w_" + str(i)]))
-            
-            interp_outputs = pd.concat([interp_outputs, pd.DataFrame(newcol, columns = ["disp_" + str(i)]), pd.DataFrame(force, columns = ["force_"+str(i)])], axis=1)
+            interp_outputs = pd.concat([interp_outputs, pd.DataFrame(newcol, columns = ["disp_" + str(i)]), pd.DataFrame(force, columns = ["force_"+str(i)]), pd.DataFrame(crosshead, columns = ["crosshead_"+str(i)])], axis=1)
     
-    interp_outputs.to_csv(in_file+"_interp.csv", sep=",", index=False)
+    interp_outputs.to_csv(in_file+"_interp_mid.csv", sep=",", index=False)
