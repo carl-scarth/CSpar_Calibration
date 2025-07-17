91ffd4813ae29542e05f770b06d027b52589e9e7
Carl Scarth <carlscarth87@gmail.com>
2025-07-17 18:44:01 +0100

    created funtion for estimating mode

--- src/utils.py
+++ src/utils.py
@@ -1,5 +1,21 @@
 # Collection of useful functions used across all examples
+import numpy as np
+import pandas as pd
+from scipy.stats import gaussian_kde
 from matplotlib import rcParams
+
+def estimate_mode(x, n_points = 10000):
+    # Estimate mode of a numpy array of random samples, belonging to some continuous
+    # distribution, using kernel density estimate
+    x_vals = np.linspace(np.min(x), np.max(x), num=n_points)
+    y_vals = gaussian_kde(x).evaluate(x_vals)
+    return x_vals[np.argmax(y_vals)]
+
+def estimate_mode_df(df, **kwargs):
+    # Estimates mode for a Pandas DataFrame, the columns of which contain samples from some 
+    # underlying continuous random variable
+    modes = pd.Series([estimate_mode(df[col], **kwargs) for col in df], index = [col for col in df])
+    return modes
 
 def set_plot_params():
     # Set commonly used plot parameters to desired values
